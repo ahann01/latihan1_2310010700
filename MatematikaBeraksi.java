c7b7eb4f2eca94e59e29191b110c9c93b622a8ab
ahann01 <ahannn.0127@gmail.com>
2025-06-12 15:14:20 +0800

    Membuat class MatematikaBeraksi

--- MatematikaBeraksi.java
+++ MatematikaBeraksi.java
@@ -1,5 +1,12 @@
 package latihan1_2310010700;
 
 public class MatematikaBeraksi {
-    
+    public static void main(String[] args) {
+        Matematika farhan = new Matematika(0,0);
+        
+        System.out.println("Hasil Penjumlahan: "+farhan.setPenjumlahan());
+        System.out.println("Hasil Pengurangan: "+farhan.setPengurangan());
+        System.out.println("Hasil Perkalian: "+farhan.setPerkalian());
+        System.out.println("Hasil Pembagian: "+farhan.setPembagian());
+    }
 }
